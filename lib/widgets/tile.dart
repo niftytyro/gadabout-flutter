42e4eac6653310b35e35ac7143825060f90b35c2
Udasi Tharani <udasitharani@gmail.com>
2021-04-10 23:23:57 +0530

    complete app

--- lib/widgets/tile.dart
+++ lib/widgets/tile.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:gadabout/constants.dart';
 import 'package:gadabout/widgets/badge.dart';
 
-class Tile extends StatelessWidget {
+class Tile extends StatefulWidget {
   Tile({
     required this.startDestination,
     required this.endDestination,
@@ -18,63 +18,83 @@ class Tile extends StatelessWidget {
   final double flightDistanceInMillionKms;
 
   @override
+  _TileState createState() => _TileState();
+}
+
+class _TileState extends State<Tile> {
+  bool tapDown = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Align(
-          alignment: Alignment.center,
-          child: CustomPaint(
-            painter: TilePainter(),
-            child: Container(
-              width: 200.0,
-              height: 270.0,
-              padding: EdgeInsets.symmetric(horizontal: 20.0),
-              child: LayoutBuilder(
-                builder: (context, constraints) {
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(height: 0.3 * constraints.maxHeight),
-                      FittedBox(
-                        child: Text(
-                          "$startDestination - $endDestination",
-                          style: getRegularStyle(),
+    return GestureDetector(
+      onTapDown: (TapDownDetails downDetails) {
+        setState(() {
+          tapDown = true;
+        });
+      },
+      onTapUp: (TapUpDetails upDetails) {
+        setState(() {
+          tapDown = false;
+        });
+      },
+      child: Stack(
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: CustomPaint(
+              painter: TilePainter(),
+              child: Container(
+                width: 200.0,
+                height: 270.0,
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: LayoutBuilder(
+                  builder: (context, constraints) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 0.3 * constraints.maxHeight),
+                        FittedBox(
+                          child: Text(
+                            "${widget.startDestination} - ${widget.endDestination}",
+                            style: getRegularStyle(),
+                          ),
                         ),
-                      ),
-                      SizedBox(height: 20.0),
-                      Text("FLIGHT DURATION",
-                          style: getRegularStyle(fontSize: 7.0)),
-                      SizedBox(height: 5.0),
-                      FittedBox(
-                        child: Text("$flightDuration DAYS",
-                            style: getRegularStyle()),
-                      ),
-                      SizedBox(height: 5.0),
-                      FittedBox(
-                        child: Text("$flightDistanceInMillionKms MILLION KM",
-                            style: getRegularStyle(
-                                fontSize: 10.0,
-                                fontColor: Colors.orange[800] as Color)),
-                      ),
-                      Badge(),
-                    ],
-                  );
-                },
+                        SizedBox(height: 20.0),
+                        Text("FLIGHT DURATION",
+                            style: getRegularStyle(fontSize: 7.0)),
+                        SizedBox(height: 5.0),
+                        FittedBox(
+                          child: Text("${widget.flightDuration} DAYS",
+                              style: getRegularStyle()),
+                        ),
+                        SizedBox(height: 5.0),
+                        FittedBox(
+                          child: Text(
+                              "${widget.flightDistanceInMillionKms} MILLION KM",
+                              style: getRegularStyle(
+                                  fontSize: 10.0,
+                                  fontColor: Colors.orange[800] as Color)),
+                        ),
+                        Badge(),
+                      ],
+                    );
+                  },
+                ),
               ),
             ),
           ),
-        ),
-        Align(
-          alignment: Alignment.topRight,
-          child: Transform.translate(
-            offset: Offset(0.0, -30.0),
-            child: Container(
-              height: 100.0,
-              child: Image.asset("assets/warship.png"),
+          Align(
+            alignment: Alignment.topRight,
+            child: Transform.translate(
+              offset: Offset(0.0, -30.0),
+              child: Container(
+                height: 100.0,
+                child: Image.asset("assets/warship.png"),
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
@@ -94,11 +114,11 @@ class TilePainter extends CustomPainter {
     canvas.drawLine(
         Offset(0, kHeight / 8), Offset(0, kHeight - kHeight / 8), paint);
     // Top Left Arc
-    canvas.drawArc(Offset(0, 0) & Size(kWidth * 2 / 6 - 5, kHeight * 2 / 8 - 5),
-        pi, pi / 2, false, paint);
+    canvas.drawArc(Offset(0, 0) & Size(kWidth * 2 / 6, kHeight * 2 / 8), pi,
+        pi / 2, false, paint);
     // Left-Bottom Connector
-    canvas.drawLine(Offset(1, kHeight - kHeight / 8 + 1),
-        Offset(kWidth / 6 - 1, kHeight - 1), paint);
+    canvas.drawLine(
+        Offset(0, kHeight - kHeight / 8), Offset(kWidth / 6, kHeight), paint);
     // Bottom Border
     canvas.drawLine(Offset(kWidth / 6, kHeight),
         Offset(kWidth - kWidth / 8, kHeight), paint);
